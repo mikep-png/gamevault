9f00de1b2e73b88f73a2ecbc75f2a6006c027834
mikep-png <ssedccarn@gmail.com>
2026-09-10 13:46:51 -0500

    Preparar backend para Render

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const path = require("path");
 
 const app = express();
-const PORT = 3002;
+const PORT = process.env.PORT || 3002;
 
 app.use(cors());
 app.use(express.json());
